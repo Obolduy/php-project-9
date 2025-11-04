360589b19423e83984ff0b42608b62aea4692cd4
Obolduy <ivanox01@yandex.ru>
2025-11-04 12:51:42 +0300

    Stub test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hexlet\Code\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testBasicAssertion(): void
+    {
+        $this->assertTrue(true);
+    }
+}
