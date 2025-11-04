86b8aba4800bd0b5ae15a6b13e6c02cc3cac3bed
Obolduy <ivanox01@yandex.ru>
2025-11-05 00:02:42 +0300

    Step 2.

--- public/index.php
+++ public/index.php
@@ -1,20 +1,26 @@
 <?php
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
+
+// Настройка Twig
+$twig = Twig::create(__DIR__ . '/../resources/views', ['cache' => false]);
+$app->add(TwigMiddleware::create($app, $twig));
 
 $app->addBodyParsingMiddleware();
 
 $app->addRoutingMiddleware();
 
 $app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write('Hello, World!');
-
-    return $response;
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'index.twig');
 });
 
 $app->run();
